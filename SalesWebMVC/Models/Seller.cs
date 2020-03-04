360d1ef43b20ebc487d6cdeb2d691cd0e649526c
BBonomi <bbaratari@gmail.com>
2020-03-04 11:49:21 -0300

    App locale, number and date formatting

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,19 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)] //cria link mailto: no campo email
         public string Email { get; set; }
+
+        [Display(Name = "Birth Date")] //customizando o nome que aparece na tela
+        [DataType(DataType.Date)] //customiza o campo data
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]//customizando o nome que aparece na tela
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; } //associação do Vendedor com Departamento (Vendedor só tem 1 departamento)
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>(); //associação do Vendedor com Vendas (1 para *)
