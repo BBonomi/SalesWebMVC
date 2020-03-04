28e72e583147e93aa7dfa295aaa6c52545bef4bc
BBonomi <bbaratari@gmail.com>
2020-03-04 11:20:15 -0300

    Returning custom error page

--- SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace SalesWebMVC.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; } //alteração para acrescentar uma menssagem customizada
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
